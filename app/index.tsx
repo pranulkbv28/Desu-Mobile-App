c4118c9f5bcbc50fe29e853822c47f0428754267
pranulkbv28 <28bvpranaav@gmail.com>
2024-11-11 12:11:53 +0530

    initiall commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#0cc0df",
+      }}
+    >
+      <Text>D</Text>
+      <Text>E</Text>
+      <Text>S</Text>
+      <Text>U</Text>
+    </View>
+  );
+}
